629d9d79fb068ca4dba0619db72eda509b918a7b
Mingyu Song <100754581+mikekks@users.noreply.github.com>
2023-05-18 09:53:42 +0900

    Add files via upload
    p.36 Delegate Chain

---
+++ Advanced2/WindowsFormsApp5/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        int num_log = 1;
+        Thread thread;
+
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Click += button_Chain;
+        }
+
+        private void button_Chain(object sender, EventArgs e)
+        {
+            (sender as Button).BackColor = System.Drawing.Color.Red;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
