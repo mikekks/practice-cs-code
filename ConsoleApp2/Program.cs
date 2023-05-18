cf788b21a8f59937566b6a5481af7f81ad379cda
Mingyu Song <100754581+mikekks@users.noreply.github.com>
2023-05-18 09:57:46 +0900

    Add files via upload
    p.42 Task 실습

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace chap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task t1 = Task.Run(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("print Async");
+            });
+
+            Console.WriteLine("print Sync");
+            t1.Wait();
+
+            Console.WriteLine("Main end");
+        }
+    }
+}
